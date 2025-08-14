a00f3b2b020b58b766db0f2757326e32cea6c375
Rahul Choudhary <rahulchoudhary19march@gmail.com>
2025-08-14 22:06:19 +0530

    string start

---
+++ 4.30pm/string/last.java
@@ -0,0 +1,20 @@
+class last{
+	public static void main(String[] args)
+	{
+		//lastOf()	  
+		String str="karnataka";
+		System.out.println(str.lastIndexOf('t'));
+		int p=str.lastIndexOf('a');
+		int q=str.lastIndexOf('a',p-1);
+		int q=str.lastIndexOf('a',q-1);
+		int q=str.lastIndexOf('a',r-1);
+		System.out.prinln(p);
+		System.out.prinln(q);
+		System.out.prinln(r);
+		System.out.prinln(s);
+		
+		
+		
+		
+	}
+}
---
+++ 4.30pm/string/str1.java
@@ -0,0 +1,39 @@
+
+//charAt is used for print the specific letter  
+
+class str1{
+	public static void main(String[] args)
+	{
+		String str ="rahul choudhary rr";
+		System.out.println(str.charAt(4));
+		System.out.println(str.charAt(16));
+		//length method is used for finding the lenght of string
+		System.out.println(str.length());
+		/*toCharArray()
+		tiger
+			->String str="Tiger" 
+			->char[] ch = str.toCharArray();*/
+		
+
+		//toCharArray() method is used
+		String str1 = "tiger";
+		char[] ch =str.toCharArray();
+		for(int i=0; i<ch.length; i++)
+		{
+			System.out.println(ch[i]);
+		}
+		
+		//indexOf() is used for searching the value .  
+
+      System.out.println(str.indexOf('r'));
+	  
+	  int a= str.indexOf('a');
+	  int b= str.indexOf('a',a+1);
+	  System.out.println(str.indexOf('a'));	
+      System.out.println(str.indexOf('b'));		
+
+		//lastOf()	  
+		String str3="karnataka";
+		System.out.println('t');
+	}
+}
