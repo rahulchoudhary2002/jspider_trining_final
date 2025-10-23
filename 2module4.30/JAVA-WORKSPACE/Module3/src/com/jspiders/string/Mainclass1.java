fa5b0a6ab564d55c88c9822f352b7e5f1f4a4fcf
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-10-23 22:30:02 +0530

    string-class

--- 2module4.30/JAVA-WORKSPACE/Module3/src/com/jspiders/string/Mainclass1.java
+++ 2module4.30/JAVA-WORKSPACE/Module3/src/com/jspiders/string/Mainclass1.java
@@ -1,5 +1,16 @@
 package com.jspiders.string;
 
+//string class object are comparable
 public class Mainclass1 {
-
+	public static void main(String[] args) {
+		String str="chennai";
+		
+		System.out.println(str.compareTo("chennai"));//0
+		
+		System.out.println(str.compareTo("delhi"));//-1
+		
+		System.out.println(str.compareTo("bangalore"));//+1
+		
+		
+	}
 }
---
+++ 2module4.30/JAVA-WORKSPACE/Module3/src/com/jspiders/string/Mainclass2.java
@@ -0,0 +1,16 @@
+package com.jspiders.string;
+//overridden methods of object class
+public class Mainclass2 {
+
+	public static void main(String[] args) {
+		String s1="Jspiders";
+		String s2="a";
+		String s3="java";
+		String s4=new String("java");
+		System.out.println(s1.toString());
+		System.out.println(s2.hashCode());
+		System.out.println(s3.equals(s4));
+
+	}
+
+}
